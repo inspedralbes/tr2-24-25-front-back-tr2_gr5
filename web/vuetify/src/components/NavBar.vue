2d09b0275b4fcb90e0e99324fbe261106b205d01
arnau <a22arnmaljoa@inspedralbes.cat>
2024-12-03 13:09:51 +0100

    Creació branca llistarIncidencies

---
+++ web/vuetify/src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar id="tabla">
+    <v-toolbar-title>
+      <v-btn to="/supportly">Supportly</v-btn>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-layout class="overflow-visible" style="height: 56px">
+      <v-bottom-navigation v-model="value" class="options" grow rounded="lg">
+        <v-btn to="/incidencies">
+          <v-icon>mdi-alert-circle-outline</v-icon>
+
+          Incidencies
+        </v-btn>
+      </v-bottom-navigation>
+    </v-layout>
+  </v-app-bar>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.options {
+  background-color: rgb(255, 251, 230);
+}
+#tabla {
+  background-color: rgb(255, 234, 167);
+}
+</style>
---
+++ web/vuetify/src/pages/incidencies.vue
@@ -0,0 +1,9 @@
+<template>
+    <IncidenciesPage /> 
+</template>
+<script setup>
+ import IncidenciesPage from '@/components/IncidenciesPage.vue';
+</script>
+<style scoped>
+
+</style>
