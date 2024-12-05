7dc8f5632fa3e5142c21364642df3a6a067c1203
a22arnmaljoa <a22arnmaljoa@inspedralbes.cat>
2024-12-05 08:18:24 +0100

    Incidencies llistades JSON

--- web/vuetify/src/components/IncidenciesPage.vue
+++ web/vuetify/src/components/IncidenciesPage.vue
@@ -1,18 +1,31 @@
 <template>
-
-<v-card
-    class="mx-auto"
-    max-width="344"
-    subtitle="hola"
-    title="hola"
-    link>
-  
+  <v-card
+    v-for="incidencia in incidencies"
+    :key="incidencia.ID"
+    class="my-5 ancho align-left"
+    :title="incidencia.Nom"
+    :subtitle="incidencia.text_descriptiu"
+    link
+  >
   </v-card>
-
 </template>
+
 <script setup>
+import { ref } from 'vue';
+import incidenciesDades from "@/assets/incidencies.json";
 
+const incidencies = ref([]);
+incidencies.value = incidenciesDades;
 </script>
-<style scoped>
 
+<style scoped>
+.ancho {
+  height: 90px;
+  width: 1300px;
+}
+
+.align-left {
+  margin-left: 25px; 
+  margin-right: auto;
+}
 </style>
