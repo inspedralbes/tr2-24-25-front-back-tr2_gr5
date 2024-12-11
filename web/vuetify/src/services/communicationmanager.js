949c29fd27176b1e521e4fc07c0449fe2740b88c
Edson <a23edstorcev@inspedralbes.cat>
2024-12-11 18:24:33 +0100

    cambio pequeño

--- web/vuetify/src/services/communicationmanager.js
+++ web/vuetify/src/services/communicationmanager.js
@@ -1,6 +1,6 @@
 export async function getPeticio() {
-    console.log('Fetching data from:', `${import.meta.env.VITE_URL_BACK}/peticion`);
-    const response = await fetch(`${import.meta.env.VITE_URL_BACK}/peticion`);
+    console.log('Fetching data from:', `${import.meta.env.VITE_URL_BACK}peticion`);
+    const response = await fetch(`${import.meta.env.VITE_URL_BACK}peticion`);
     if (!response.ok) {
       throw new Error('Error al obtenir dades');
     }
