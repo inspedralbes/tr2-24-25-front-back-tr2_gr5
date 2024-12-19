d3398d454097c367ae9d9e970d059a46bf32f656
Edson <a23edstorcev@inspedralbes.cat>
2024-12-19 22:02:22 +0100

    bd

--- backend/data/createDB.sql
+++ backend/data/createDB.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS categoria (
   nom VARCHAR(100) NOT NULL UNIQUE
 );
 
-<<<<<<< HEAD
 CREATE TABLE IF NOT EXISTS curs (
   id_curs INT(11) NOT NULL PRIMARY KEY,
   numero_curs ENUM('1', '2') NOT NULL,
@@ -57,67 +56,6 @@ CREATE TABLE IF NOT EXISTS resposta (
   FOREIGN KEY (id_usuari) REFERENCES usuaris (id_usuari) ON DELETE CASCADE
 );
 
-=======
-
-CREATE TABLE IF NOT EXISTS curs (
-  id_curs INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  numero_curs ENUM('1', '2') NOT NULL,
-  nom_curs ENUM('SMIX', 'DAM', 'DAW', 'ASIX') NOT NULL
-);
-
-
-CREATE TABLE IF NOT EXISTS usuaris (
-  id_usuari INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  nom VARCHAR(100) NOT NULL,
-  cognom VARCHAR(100) NOT NULL,
-  correu_alumne VARCHAR(100) UNIQUE,
-  correu_tutor VARCHAR(100) UNIQUE,
-  correu_profe VARCHAR(100),
-  id_curs INT(11),
-  contrasenya VARCHAR(100) NOT NULL,
-  tipus ENUM('alum', 'prof', 'ment') NOT NULL,
-  imatge_usuari_ruta VARCHAR(255) DEFAULT NULL,
-  valid_tut_aula TINYINT(1) DEFAULT '0' NOT NULL,
-  valid_tut_legal TINYINT(1) DEFAULT '0',
-  FOREIGN KEY (id_curs) REFERENCES curs (id_curs) ON DELETE CASCADE
-);
-
-
-CREATE TABLE IF NOT EXISTS coneixements (
-  id_coneixement INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  id_usuari INT(11) NOT NULL,
-  id_categoria INT(11) NOT NULL,
-  FOREIGN KEY (id_usuari) REFERENCES usuaris (id_usuari) ON DELETE CASCADE,
-  FOREIGN KEY (id_categoria) REFERENCES categoria (id_categoria) ON DELETE CASCADE
-);
-
-
-CREATE TABLE IF NOT EXISTS peticio (
-  id_peticio INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  id_usuari INT(11) NOT NULL,
-  id_categoria INT(11) NOT NULL,
-  nom_peticio VARCHAR(100) NOT NULL,
-  descripcio TEXT NOT NULL,
-  activado BOOLEAN NOT NULL DEFAULT FALSE,
-  data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (id_usuari) REFERENCES usuaris (id_usuari) ON DELETE CASCADE,
-  FOREIGN KEY (id_categoria) REFERENCES categoria (id_categoria) ON DELETE CASCADE
-);
-
-
-CREATE TABLE IF NOT EXISTS resposta (
-  id_resposta INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  id_peticio INT(11) NOT NULL,
-  id_usuari INT(11) NOT NULL,
-  id_resposta_ref INT(11) DEFAULT 0,
-  contingut TEXT NOT NULL,
-  data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (id_peticio) REFERENCES peticio (id_peticio) ON DELETE CASCADE,
-  FOREIGN KEY (id_usuari) REFERENCES usuaris (id_usuari) ON DELETE CASCADE
-);
-
-
->>>>>>> c80622619e65b621c71b5ed2e51d0f6211c7c1f1
 CREATE TABLE IF NOT EXISTS valoracio (
   id_valoracio INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   id_peticio INT(11) NOT NULL,
